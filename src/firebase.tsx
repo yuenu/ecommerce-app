598ab145b1b460d6bc88c4a1055caef1c9db64e9
yuenu <miraiku82@gmail.com>
2022-04-27 00:11:36 +0800

    chore: move firebase config to environment variable

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBBGX0P9x92tojjv7GQO8yNpG7N_9jkUXk',
-  authDomain: 'react-widget-11077.firebaseapp.com',
-  projectId: 'react-widget-11077',
-  storageBucket: 'react-widget-11077.appspot.com',
-  messagingSenderId: '456281919971',
-  appId: '1:456281919971:web:472a864b97b946dbf8d0d6',
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTO_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGE_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APPID}`,
 }
 
 // Initialize Firebase
